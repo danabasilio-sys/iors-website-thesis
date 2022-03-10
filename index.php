4d81d54f3a60715808511f65083700293feb0e4c
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-03-10 21:10:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
 
     <div>
 
-      <h1 class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">In our red
+      <h1 class="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">In our pink
 
       <span class="d-block fs-700 ff-serif text-white">Stilettos</span></h1>
 
