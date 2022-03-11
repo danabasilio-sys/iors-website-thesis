d517b0fba16498d416f7ddfd262fbad874591bfd
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-11 14:39:30 +0800

    Update archive.php

--- admin/archive.php
+++ admin/archive.php
@@ -117,8 +117,8 @@ if(isset($_GET['did'])){
             text: "You won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
             confirmButtonText: 'Confirm delete'
         }).then((result) => {
             if (result.isConfirmed) {
