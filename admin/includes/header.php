640fd717a7580da18e898aa64c99602e4476e3e6
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-14 04:11:25 +0800

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -14,8 +14,8 @@ $cur = $cur[0];
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" sizes="32x32" href="dist/img/favicon-stiletto.png">
-    <title>Admin Panel</title>
+    <link rel="icon" type="image/png" href="dist/img/favicon-stiletto.png">
+    <title>IORS Admin Panel</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
