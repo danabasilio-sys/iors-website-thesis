6e29e81afb27f769db2e9016d58e81b315d1f675
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-14 04:11:51 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -55,6 +55,7 @@ if(isset($_SESSION['uid']) && intval($_SESSION['uid']) > 0 && isset($_SESSION['i
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Panel</title>
+    <link rel="icon" type="image/png" href="dist/img/favicon-stiletto.png">
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
