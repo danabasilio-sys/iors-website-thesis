94d85d48d943d07c4c088bb71313c77ac01bb15b
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-14 04:12:13 +0800

    Update recover-password.php

--- admin/recover-password.php
+++ admin/recover-password.php
@@ -91,7 +91,7 @@ if(isset($_POST['recover']))
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin Panel</title>
-
+  <link rel="icon" type="image/png" href="dist/img/favicon-stiletto.png">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
